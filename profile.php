bbb511deab9659bdb0be50eb56282d2660bd0457
whitespace127 <fennxfoci@gmail.com>
2018-03-28 14:48:54 +0700

    Update profile.php

--- profile.php
+++ profile.php
@@ -4,12 +4,12 @@ $bearer = 'gO9loblB5FjTffIbSm/JdZxwA1uZlwQbpmAIis6oWHWqB1g/C6F34UIN2yn29V7R0/tVC
 //$url = 'https://api.line.me/v2/bot/profile/' . $userid;
 $url = 'https://api.line.me/v2/bot/profile/fennxfoci';
 //$headers = array('Authorization: Bearer ' . $bearer);
-$headers = array('Authorization: Bearer JVc7NIOEz1KDqtec2VfpoVK8m1f7krwhB1hXNWBn3Ncsn6UQ+KZ03QyqQWlDg0gz0/tVCzRgEAn1Nyp+Vb2QaUMkoLxxzxLYGWUzz+M1pS7Glo0orHx89grcKIWj+wxKBu7EnXdb/bc3pgzu7JTS349PbdgDzCFqoOLOYbqAITQ=');
+$headers = array('Authorization: Bearer uOaAKl6jjb1EOsEmO5tHrplj3sRJUIlCeYWjDGVeQBW9b7fx2ab2rK9E6E3OOZdU0/tVCzRgEAn1Nyp+Vb2QaUMkoLxxzxLYGWUzz+M1pS4XUwdidk0QbkyZXGznJWsoJ+bBvjpWEyJIBdZlzw2unY9PbdgDzCFqoOLOYbqAITQ=');
 
 $ch = curl_init($url);
 curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-curl_setopt($ch, CURLOPT_GET, 1);
 curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+curl_setopt($ch, CURLOPT_GET, 1);
 
 curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
 			
