92edd6f5a78cbd0d5e505c15d08577bb441fbd10
whitespace127 <fennxfoci@gmail.com>
2018-03-28 14:21:21 +0700

    Update profile.php

--- profile.php
+++ profile.php
@@ -1,7 +1,8 @@
 <?php
 $userid = 'fennxfoci';
 $bearer = 'gO9loblB5FjTffIbSm/JdZxwA1uZlwQbpmAIis6oWHWqB1g/C6F34UIN2yn29V7R0/tVCzRgEAn1Nyp+Vb2QaUMkoLxxzxLYGWUzz+M1pS4h0FvMK82Hdb2grrg6hO/m9qMMW++evdahTcbD9hrB2o9PbdgDzCFqoOLOYbqAITQ=';
-$url = 'https://api.line.me/v2/bot/profile/' . $userid;
+//$url = 'https://api.line.me/v2/bot/profile/' . $userid;
+$url = 'https://api.line.me/v2/bot/profile/fennxfoci';
 $headers = array('Authorization: Bearer ' . $bearer);
 
 $ch = curl_init($url);
