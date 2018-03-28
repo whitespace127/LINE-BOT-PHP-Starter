af74d80086b4f21b577928cfbb84934f915f7a3a
whitespace127 <fennxfoci@gmail.com>
2018-03-28 14:39:02 +0700

    Update profile.php

--- profile.php
+++ profile.php
@@ -2,9 +2,9 @@
 $userid = 'fennxfoci';
 $bearer = 'gO9loblB5FjTffIbSm/JdZxwA1uZlwQbpmAIis6oWHWqB1g/C6F34UIN2yn29V7R0/tVCzRgEAn1Nyp+Vb2QaUMkoLxxzxLYGWUzz+M1pS4h0FvMK82Hdb2grrg6hO/m9qMMW++evdahTcbD9hrB2o9PbdgDzCFqoOLOYbqAITQ=';
 //$url = 'https://api.line.me/v2/bot/profile/' . $userid;
-$url = 'https://api.line.me/v2/bot/profile/U69dfdebb75c8d6aaef5b6b440154115e';
+$url = 'https://api.line.me/v2/bot/profile/fennxfoci';
 //$headers = array('Authorization: Bearer ' . $bearer);
-$headers = array('Authorization: Bearer gO9loblB5FjTffIbSm/JdZxwA1uZlwQbpmAIis6oWHWqB1g/C6F34UIN2yn29V7R0/tVCzRgEAn1Nyp+Vb2QaUMkoLxxzxLYGWUzz+M1pS4h0FvMK82Hdb2grrg6hO/m9qMMW++evdahTcbD9hrB2o9PbdgDzCFqoOLOYbqAITQ=');
+$headers = array('Authorization: Bearer JVc7NIOEz1KDqtec2VfpoVK8m1f7krwhB1hXNWBn3Ncsn6UQ+KZ03QyqQWlDg0gz0/tVCzRgEAn1Nyp+Vb2QaUMkoLxxzxLYGWUzz+M1pS7Glo0orHx89grcKIWj+wxKBu7EnXdb/bc3pgzu7JTS349PbdgDzCFqoOLOYbqAITQ=');
 
 $ch = curl_init($url);
 curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
