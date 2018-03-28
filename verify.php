9b396b4133d7cfa4b196ad7cd95b106c675f3dc7
whitespace127 <fennxfoci@gmail.com>
2018-03-28 13:49:01 +0700

    Add files via upload

--- verify.php
+++ verify.php
@@ -1,11 +1,21 @@
 <?php
-$access_token = 'zy9bDK/VoakRWSYKudJrh4CxL0Nlcyvz9tHJ6bLmUIPx0CiRiSmIAArfbqFpeGFd0/tVCzRgEAn1Nyp+Vb2QaUMkoLxxzxLYGWUzz+M1pS7bHHBL4+fufL0wCTLbwqY8wnrusEk4o3DjJJHNwvQBggdB04t89/1O/w1cDnyilFU=';
-$url = 'https://api.line.me/v1/oauth/verify';
-$headers = array('Authorization: Bearer ' . $access_token);
+//$access_token = 'zy9bDK/VoakRWSYKudJrh4CxL0Nlcyvz9tHJ6bLmUIPx0CiRiSmIAArfbqFpeGFd0/tVCzRgEAn1Nyp+Vb2QaUMkoLxxzxLYGWUzz+M1pS7bHHBL4+fufL0wCTLbwqY8wnrusEk4o3DjJJHNwvQBggdB04t89/1O/w1cDnyilFU=';
+$clientid = '1570757358';
+$clientsecret = '63027787afefca74c046df98f144a3df';
+$url = 'https://api.line.me/v2/oauth/accessToken';
+$headers = array('Content-Type:application/x-www-form-urlencoded');
+$body = {
+	'grant_type':'client_credentials',
+	'client_id':  $clientid,
+	'client_secret': $clientsecret
+};
 $ch = curl_init($url);
 curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
 curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+curl_setopt($ch, CURLOPT_POSTFIELDS, $body);
+
 curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
+			
 $result = curl_exec($ch);
 curl_close($ch);
 echo $result;
